b0574a92c8d2d7d3d318fd2415d56435f4fb986b
Alexander Martins <alexandermartins123@icloud.com>
2024-03-27 23:09:46 +0100

    made an update

--- controllers/task.js
+++ controllers/task.js
@@ -87,26 +87,6 @@ const deleteTaskq = async (req, res, next) => {
     next(error);
   }
 };
-const deleteTaskw = async (req, res, next) => {
-  try {
-    const {
-      user: { userId },
-      params: { id: taskId },
-    } = req;
-
-    const task = await Task.findByIdAndDelete({
-      _id: taskId,
-      createdBy: userId,
-    });
-
-    if (!task) {
-      throw new NotFoundError(`No job with id ${taskId}`);
-    }
-    res.status(200).json({ msg: "delete task successfully" });
-  } catch (error) {
-    next(error);
-  }
-};
 
 const deleteTask = async (req, res, next) => {
   try {
